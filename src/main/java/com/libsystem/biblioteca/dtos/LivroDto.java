a85c46220775ddb749f58f2f8d36296bfe150d39
Isabela <isabela.moreira8721@gmail.com>
2025-04-02 21:38:32 -0300

    criacao dto livro

---
+++ src/main/java/com/libsystem/biblioteca/dtos/LivroDto.java
@@ -0,0 +1,31 @@
+package com.libsystem.biblioteca.dtos;
+
+public class LivroDto {
+	private String titulo;
+	private String autor;
+	private String editora;
+	private String anoPublicacao;
+	private String status;
+	private Integer edicao;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public String getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public Integer getEdicao() {
+		return edicao;
+	}
+
+	
+}
